21f039c59148fea408cb34df0b6e98cd3c7dc5cf
akshit_wadhwa <akshitwadhwa06@gmail.com>
2025-01-26 13:27:44 +0530

    Update 2127_26jan.py

--- 2127_26jan.py
+++ 2127_26jan.py
@@ -1,1 +1,7 @@
+-- In this question there a n number of employees who want to be seated at a circular table, 
 
+this is a directed graph problem where we have to connect one person with it given favourite person and seat them across from
+their favorite person.
+Now the problem is that we have to sit one or the other adjoining to each other.
+So starting of with calculating the distance with between the two nodes which will be 
+ 
